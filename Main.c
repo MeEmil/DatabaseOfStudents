20a12028807ffba06a0f9efe3834201620aa20c1
MeEmil <eolsson.olsson99@gmail.com>
2018-12-21 20:46:24 +0100

    Added a new Main.c file that has the writer to files saved on the
    computer in it

---
+++ Main.c
@@ -0,0 +1,21 @@
+int main()
+{
+   // printf() displays the string inside quotation
+   FILE *f = fopen("file.txt", "w");
+ if (f == NULL)
+ {
+  printf("Error opening file!\n");
+ }
+ /* print some text */
+ const char *text = "Write this to file";
+ fprintf(f, "Some text: %s\n", text);
+ /* print integers and floats */
+ int i = 1;
+ float py = 3.1415927;
+ fprintf(f, "Integer: %d, float: %f\n", i, py);
+ /* printing single chatacters */
+ char c = 'A';
+ fprintf(f, "A character: %c\n", c);
+ fclose(f);
+ getchar();
+}
